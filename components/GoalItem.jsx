fbd031fc37fda0246f33c7b956044c56d1d6e98b
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-06-17 22:25:53 -0300

    33. Adding an Android Ripple Effect & an iOS Alternative

--- components/GoalItem.jsx
+++ components/GoalItem.jsx
@@ -2,13 +2,17 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {    
     return (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-            <View style={styles.goalItemWrapper}>
+        <View style={styles.goalItemWrapper}>
+            <Pressable 
+                android_ripple={{color: "##5c07c9"}}
+                onPress={props.onDeleteItem.bind(this, props.id)}>
+            
                 <Text style={styles.goalItem}>
                     {props.text}
                 </Text>
-            </View>
-        </Pressable>
+            
+            </Pressable>
+        </View>
     )
 }
 
@@ -18,11 +22,11 @@ const styles = StyleSheet.create({
     goalItemWrapper: {
       margin: 8,
       borderRadius: 6,
-      backgroundColor: '#5e0acc',
-      padding: 8,
+      backgroundColor: '#5e0acc',      
     },
     goalItem: {
       color: 'white',
       textAlign: 'center',
+      padding: 8,
     }
 });
